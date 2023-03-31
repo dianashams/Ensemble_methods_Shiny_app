fc533e705800ed1d2e0b44016cf25887aac1ddd0
Diana <bigdatawithdiana@gmail.com>
2023-03-31 22:15:38 +0100

    Adding Diabetes W500 data

--- app.R
+++ app.R
@@ -20,11 +20,11 @@ library(GGally)
 #setwd("C:/Users/K1514174/OneDrive - King's College London/Desktop/Ensemble methods/ensemble-methods-for-survival-analysis-main")
 
 elsa_file= "Data/diabetes_data_for_method.csv"
+elsa_file_2= "Data/SZ_Diabetes_SELF_imputed_for_prediction_PhDStudy3.csv"
 foot_file = "Data/5yearfoot_ensemble.csv"
 hnscc_file = "Data/hnscc_merged.csv"
-w500_file = "Data/whas500.csv"
+w500_file = "Data/W500.csv"
 gbsg_data = "Data/gbsg_data.csv"
-
 
 source("R_Simulating_survival_data.R")
 source("Ensemble_Methods.R")
@@ -70,9 +70,11 @@ ui <- fluidPage(
                     "Simulated: Non-linear", 
                     "Simulated: Linear",
                     "Simulated: Non PH",
-                    "Diabetes_depression",
+                    "Foot_Ulcer_Study",
+                    "ELSA_Diabetes_Study2",
                     "ELSA_Diabetes",
                     "Hnscc",
+                    "W500",
                     "Custom"),
       ),
       numericInput(inputId = "fixed_time",
@@ -259,14 +261,15 @@ server <- function(input, output) {
                                     randomseed = input$randomseed,
                                     percentcensored = 1- input$percent_censored_inv,
                                     drop_out = input$drop_out_rate),
-           
+           "ELSA_Diabetes_Study2" = read.csv(elsa_file_2),
            "ELSA_Diabetes" = read.csv(elsa_file), 
-           "Diabetes_depression" = read.csv(foot_file),
+           "Foot_Ulcer_Study" = read.csv(foot_file),
            "Custom" = read.csv(input$custom_file),
-           "Hnscc" = read.csv(hnscc_file)
+           "Hnscc" = read.csv(hnscc_file),
+           "W500" = read.csv(w500_file)
     )
   })
-  
+  cat(model_e_0 ,sep = '", "')
   ### define predictors 
   predictfactors <- reactive({
     switch(input$data_type,
@@ -277,10 +280,18 @@ server <- function(input, output) {
            "ELSA_Diabetes" = c("baseline_age_", "genderdum", "bmi_0_", "cvd_0", "hyp_0",
                                "baseline_exercise",  "B_wealth","baseline_B_smokstatus",
                                "t2dm_", "pc1_", "pc2_", "pc3_"),
-           "Diabetes_depression"  = c("age", "sex", "texsev", "mean_hbamva",
+           "ELSA_Diabetes_Study2" = c("sz20_", "pc1_", "pc2_", "pc3_", "pc4_", "age_", 
+                                     "sex", "bmi_0_", "hyp_0", "cvd_0", "B_dep_0", 
+                                     "trig_0", "baseline_hdl", "stroke_0", "B_smokstatus_0", 
+                                     "exercise_light", "exercise_vig", "EduLevel_low",
+                                     "EduLevel_med", "wealth_med", "wealth_low", "t2dm_"),
+           "Foot_Ulcer_Study"  = c("age", "sex", "texsev", "mean_hbamva",
                                       "anydep",  "sdscas", "socclas3"),
            "Hnscc" = c("original_firstorder_10Percentile" ,"original_firstorder_90Percentile" ,"original_firstorder_Energy" ,"original_firstorder_Entropy" ,"original_firstorder_InterquartileRange" ,"original_firstorder_Kurtosis" ,"original_firstorder_Maximum" ,"original_firstorder_Mean" ,"original_firstorder_MeanAbsoluteDeviation" ,"original_firstorder_Median" ,"original_firstorder_Minimum" ,"original_firstorder_Range" ,"original_firstorder_RobustMeanAbsoluteDeviation" ,"original_firstorder_RootMeanSquared" ,"original_firstorder_Skewness" ,"original_firstorder_TotalEnergy" ,"original_firstorder_Uniformity" ,"original_firstorder_Variance" ,"original_glcm_Autocorrelation" ,"original_glcm_ClusterProminence" ,"original_glcm_ClusterShade" ,"original_glcm_ClusterTendency" ,"original_glcm_Contrast" ,"original_glcm_Correlation" ,"original_glcm_DifferenceAverage" ,"original_glcm_DifferenceEntropy" ,"original_glcm_DifferenceVariance" ,"original_glcm_Id" ,"original_glcm_Idm" ,"original_glcm_Idmn" ,"original_glcm_Idn" ,"original_glcm_Imc1" ,"original_glcm_Imc2" ,"original_glcm_InverseVariance" ,"original_glcm_JointAverage" ,"original_glcm_JointEnergy" ,"original_glcm_JointEntropy" ,"original_glcm_MCC" ,"original_glcm_MaximumProbability" ,"original_glcm_SumAverage" ,"original_glcm_SumEntropy" ,"original_glcm_SumSquares" ,"original_gldm_DependenceEntropy" ,"original_gldm_DependenceNonUniformity" ,"original_gldm_DependenceNonUniformityNormalized" ,"original_gldm_DependenceVariance" ,"original_gldm_GrayLevelNonUniformity" ,"original_gldm_GrayLevelVariance" ,"original_gldm_HighGrayLevelEmphasis" ,"original_gldm_LargeDependenceEmphasis" ,"original_gldm_LargeDependenceHighGrayLevelEmphasis" ,"original_gldm_LargeDependenceLowGrayLevelEmphasis" ,"original_gldm_LowGrayLevelEmphasis" ,"original_gldm_SmallDependenceEmphasis" ,"original_gldm_SmallDependenceHighGrayLevelEmphasis" ,"original_gldm_SmallDependenceLowGrayLevelEmphasis" ,"original_glrlm_GrayLevelNonUniformity" ,"original_glrlm_GrayLevelNonUniformityNormalized" ,"original_glrlm_GrayLevelVariance" ,"original_glrlm_HighGrayLevelRunEmphasis" ,"original_glrlm_LongRunEmphasis" ,"original_glrlm_LongRunHighGrayLevelEmphasis" ,"original_glrlm_LongRunLowGrayLevelEmphasis" ,"original_glrlm_LowGrayLevelRunEmphasis" ,"original_glrlm_RunEntropy" ,"original_glrlm_RunLengthNonUniformity" ,"original_glrlm_RunLengthNonUniformityNormalized" ,"original_glrlm_RunPercentage" ,"original_glrlm_RunVariance" ,"original_glrlm_ShortRunEmphasis" ,"original_glrlm_ShortRunHighGrayLevelEmphasis" ,"original_glrlm_ShortRunLowGrayLevelEmphasis" ,"original_glszm_GrayLevelNonUniformity" ,"original_glszm_GrayLevelNonUniformityNormalized" ,"original_glszm_GrayLevelVariance" ,"original_glszm_HighGrayLevelZoneEmphasis" ,"original_glszm_LargeAreaEmphasis" ,"original_glszm_LargeAreaHighGrayLevelEmphasis" ,"original_glszm_LargeAreaLowGrayLevelEmphasis" ,"original_glszm_LowGrayLevelZoneEmphasis" ,"original_glszm_SizeZoneNonUniformity" ,"original_glszm_SizeZoneNonUniformityNormalized" ,"original_glszm_SmallAreaEmphasis" ,"original_glszm_SmallAreaHighGrayLevelEmphasis" ,"original_glszm_SmallAreaLowGrayLevelEmphasis" ,"original_glszm_ZoneEntropy" ,"original_glszm_ZonePercentage" ,"original_glszm_ZoneVariance" ,"original_ngtdm_Busyness" ,"original_ngtdm_Coarseness" ,"original_ngtdm_Complexity" ,"original_ngtdm_Contrast" ,"original_ngtdm_Strength" ,"original_shape_Elongation" ,"original_shape_Flatness" ,"original_shape_LeastAxisLength" ,"original_shape_MajorAxisLength" ,"original_shape_Maximum2DDiameterColumn" ,"original_shape_Maximum2DDiameterRow" ,"original_shape_Maximum2DDiameterSlice" ,"original_shape_Maximum3DDiameter" ,"original_shape_MeshVolume" ,"original_shape_MinorAxisLength" ,"original_shape_Sphericity" 
                        ,"original_shape_SurfaceArea" ,"original_shape_SurfaceVolumeRatio" ,"original_shape_VoxelVolume"),
+           "W500" = c("age", "gender", "hr", "sysbp", "diasbp",
+                      "bmi","cvd",  "afb","sho","chf",
+                      "av3","miord", "mitype", "los"),
            "Custom" = Clean_String(input$custom_predictors),
     )
   })
